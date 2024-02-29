b51b707f1cf74db1da5762bff6a3ac71f126c23f
d.nishpal <danilnishpal512@gmail.com>
2024-02-29 11:16:03 +0300

    fix

--- __init__.py
+++ __init__.py
@@ -1,1 +1,15 @@
 import aiogram
+from aiogram import types
+from aiogram import client
+from aiogram import dispatcher
+from aiogram import enums
+from aiogram import filters
+from aiogram import fsm
+from aiogram import handlers
+from aiogram import methods
+from aiogram import utils
+from aiogram import webhook
+__all__ = (
+    "aiogram", "types", "client", "dispatcher", "enums", "filters",
+    "fsm", "handlers", "methods", "utils", "webhook",
+)
